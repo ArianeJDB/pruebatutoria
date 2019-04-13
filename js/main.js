5c2ca1296a9b0057d4bbde38ea461d10ff88e0f8
arijdb <ariane.jurado@gmail.com>
2019-04-13 12:47:08 +0200

    indent and deleted json

--- js/main.js
+++ js/main.js
@@ -1,34 +1,30 @@
-'use strict';
+"use strict";
 
 const terms = [
   {
-    text: 'pizza',
+    text: "pizza",
     total: 10
   },
   {
-    text: 'patata',
+    text: "patata",
     total: 2
   },
   {
-    text: 'pegatina',
+    text: "pegatina",
     total: 100
   }
 ];
 
 const ten = (str, veces) => {
-for (let i = 0; i < veces; i++) {
-  console.log(str);
-}
-}
+  for (let i = 0; i < veces; i++) {
+    console.log(str);
+  }
+};
 
+const nueva = terms => {
+  for (const item of terms) {
+    ten(item.text, item.total);
+  }
+};
 
-const nueva = (terms) => {
-  
-for (const item of terms) {
-  ten(item.text, item.total); 
-
-}
-} 
 nueva(terms);
-
-
