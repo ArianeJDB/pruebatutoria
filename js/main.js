13605f708810cb06e02944b8ea9f504106460eaa
arijdb <ariane.jurado@gmail.com>
2019-04-14 10:37:35 +0200

    fixed

--- js/main.js
+++ js/main.js
@@ -1,35 +1,23 @@
 "use strict";
 
-// const terms = [
-//   {
-//     text: "pizza",
-//     total: 10
-//   },
-//   {
-//     text: "patata",
-//     total: 2
-//   },
-//   {
-//     text: "pegatina",
-//     total: 100
-//   }
-// ];
 const api =
   "https://raw.githubusercontent.com/oneeyedman/terms/master/terms.js";
 
-const writeMe = () => {
-  fetch(api)
-    .then(response => response.json())
-    .then(data => {
-      console.log("array de obj", data.results);
-    });
-  const writeThisWord = (str, n) => {
-    for (let i = 0; i < n; i++) {
-      console.log(str);
-    }
-  };
-  for (const item of api) {
+fetch(api)
+  .then(response => response.json())
+  .then(data => {
+    console.log(writeMe(data.results));
+  });
+
+const writeThisWord = (str, n) => {
+  for (let i = 0; i < n; i++) {
+    console.log(str);
+  }
+};
+
+const writeMe = arr => {
+  for (const item of arr) {
     writeThisWord(item.text, item.total);
   }
 };
-console.log(writeMe());
+
