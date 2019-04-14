a8fe6ec25b661494ed784e6489246ef127b54c2b
arijdb <ariane.jurado@gmail.com>
2019-04-14 10:12:48 +0200

    indent

--- js/main.js
+++ js/main.js
@@ -14,24 +14,22 @@
 //     total: 100
 //   }
 // ];
-const api = 'https://raw.githubusercontent.com/oneeyedman/terms/master/terms.js';
+const api =
+  "https://raw.githubusercontent.com/oneeyedman/terms/master/terms.js";
 
 const writeMe = () => {
-fetch(api)
-  .then(response => response.json())
-  .then(data =>  {
-      console.log('array de obj', data.results);
-  })
-
+  fetch(api)
+    .then(response => response.json())
+    .then(data => {
+      console.log("array de obj", data.results);
+    });
   const writeThisWord = (str, n) => {
     for (let i = 0; i < n; i++) {
       console.log(str);
     }
-    };
-
+  };
   for (const item of api) {
     writeThisWord(item.text, item.total);
-    }
+  }
 };
-
 console.log(writeMe());
