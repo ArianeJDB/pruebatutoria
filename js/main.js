4b169f74ee67870b6d8b505bce9c4784160c9360
arijdb <ariane.jurado@gmail.com>
2019-04-14 10:04:36 +0200

    fetch

--- js/main.js
+++ js/main.js
@@ -1,30 +1,37 @@
 "use strict";
 
-const terms = [
-  {
-    text: "pizza",
-    total: 10
-  },
-  {
-    text: "patata",
-    total: 2
-  },
-  {
-    text: "pegatina",
-    total: 100
-  }
-];
+// const terms = [
+//   {
+//     text: "pizza",
+//     total: 10
+//   },
+//   {
+//     text: "patata",
+//     total: 2
+//   },
+//   {
+//     text: "pegatina",
+//     total: 100
+//   }
+// ];
+const api = 'https://raw.githubusercontent.com/oneeyedman/terms/master/terms.js';
 
-const writeThisWord = (str, n) => {
-  for (let i = 0; i < n; i++) {
-    console.log(str);
-  }
-};
+const writeMe = () => {
+fetch(api)
+  .then(response => response.json())
+  .then(data =>  {
+      console.log('array de obj', data.results);
+  })
 
-const writeMe = arr => {
-  for (const item of arr) {
+  const writeThisWord = (str, n) => {
+    for (let i = 0; i < n; i++) {
+      console.log(str);
+    }
+    };
+
+  for (const item of api) {
     writeThisWord(item.text, item.total);
-  }
+    }
 };
 
-writeMe(terms);
+console.log(writeMe());
